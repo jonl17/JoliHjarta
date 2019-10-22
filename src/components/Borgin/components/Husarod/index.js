e87ccc67ff19bb16ba1d96466e49744b07036ea2
Nonni Linux <jglorange@gmail>
2019-10-22 22:30:30 +0000

    houses

---
+++ src/components/Borgin/components/Husarod/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import HusSVG from "../../../../../static/myndir/husarod.svg"
+import "./index.css"
+const Husarod = () => {
+  return <HusSVG></HusSVG>
+}
+
+export default Husarod
--- src/components/Borgin/index.js
+++ src/components/Borgin/index.js
@@ -3,11 +3,13 @@ import { Background } from "./Styled"
 
 /* import city components */
 import Mani from "./components/Mani"
+import Husarod from "./components/Husarod"
 
 const Borgin = () => {
   return (
     <Background>
       <Mani></Mani>
+      <Husarod></Husarod>
     </Background>
   )
 }
