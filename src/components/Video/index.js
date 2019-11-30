4ebc3e0af58e2157bb46475308176cde9fcc3646
Nonni Linux <jglorange@gmail>
2019-11-30 18:48:10 +0000

    vidjo

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -3,50 +3,41 @@ import { Container, Player, Source } from "./Styled"
 import { connect } from "react-redux"
 import { triggerVideoFullscreen } from "../../state/action"
 import ExitBTN from "../ExitBTN"
-import loadGIF from "../../../static/myndir/load.gif"
+import Vimeo from "@vimeo/player"
 
 class Video extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      video: undefined,
-    }
-  }
-  /*
-  componentDidMount() {
-    let video = document.getElementById("video-player")
-    this.setState({
-      video: video,
-    })
-  }
   componentDidUpdate() {
     document
-      .getElementById("video-source")
+      .getElementById("video-player")
       .setAttribute("src", this.props.vidjo.vidjourl)
-    if (this.props.videoFullscreen) {
-      this.state.video.play()
-    } else {
-      this.state.video.pause()
-    }
-  } */
+  }
+  dispatches(dispatch) {
+    dispatch(triggerVideoFullscreen())
+    var iframe = document.getElementById("video-player")
+    var player = new Vimeo(iframe)
+    player.pause()
+  }
   render() {
     const { vidjo, platform, videoFullscreen, dispatch } = this.props
     return (
-      <Container platform={platform} open={videoFullscreen ? "100%" : "0%"}>
-        <ExitBTN
-          small
-          relativeToVideo
-          click={() => dispatch(triggerVideoFullscreen())}
-        ></ExitBTN>
-        <Player
-          src={vidjo.vidjourl}
-          height={videoFullscreen ? "100%" : "0%"}
-          width={"100%"}
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-          allowfullscreen
-        ></Player>
-      </Container>
+      <>
+        <Container platform={platform} open={videoFullscreen ? "100%" : "0%"}>
+          <ExitBTN
+            small
+            relativeToVideo
+            click={() => this.dispatches(dispatch)}
+          ></ExitBTN>
+          <Player
+            id="video-player"
+            src={vidjo.vidjourl + "?api=1"}
+            height={videoFullscreen ? "100%" : "0%"}
+            width={"100%"}
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          ></Player>
+        </Container>
+      </>
     )
   }
 }
