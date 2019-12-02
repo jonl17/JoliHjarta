615a97c71da5c35d95d049102db471972cc190ec
Nonni Linux <jglorange@gmail>
2019-12-02 14:35:26 +0000

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,5 +77,11 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      },
+    },
   ],
 }
