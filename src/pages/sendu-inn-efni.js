d8ea44b510ded6a52a03932d828100734fd6f28b
Nonni Linux <jglorange@gmail>
2019-12-03 16:30:23 +0000

    formið mobile

--- src/pages/sendu-inn-efni.js
+++ src/pages/sendu-inn-efni.js
@@ -10,13 +10,9 @@ const Wrap = styled.div`
 `
 
 const Iframe = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
   height: 100%;
   border: 0;
-  /* -webkit-overflow-scrolling: touch; */
 `
 
 const SenduInnEfni = () => {
